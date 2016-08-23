5bbdcb1f5ba69cd5a109d2c424740b91a9cc85b8
Kiri23 <christian_nogueras94@hotmail.com>
2016-08-23 19:27:43 -0400

    Initial Commit

---
+++ Raspberry_pi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raspberry_pi
+//
+//  Created by Christian Nogueras on 08/23/16.
+//  Copyright © 2016 com.christian.nogueras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
