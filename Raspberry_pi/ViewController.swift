83ee2e93d732942ef4e3e9cfb3a2009c720d6980
Kiri23 <christian_nogueras94@hotmail.com>
2016-08-27 08:40:57 -0400

    Document the view Controller File
    Add the first block of Comments to this viewController.swift

--- Raspberry_pi/ViewController.swift
+++ Raspberry_pi/ViewController.swift
@@ -2,6 +2,9 @@
 //  ViewController.swift
 //  Raspberry_pi
 //
+//  This class send event to Firebase to controll the diferente stage of the Raspberry Pi car robot
+//  and also control the UI for the iphone app(Client Side)
+//
 //  Created by Christian Nogueras on 08/23/16.
 //  Copyright © 2016 com.christian.nogueras. All rights reserved.
 //
@@ -15,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /// Call the method to runn the motor foward
         led("ON")
     }
 
@@ -22,10 +26,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func led(state: String){
+    /**
+       Run or Stop the motor
+       -author: 
+        Christian Nogueras
+       -parameters: 
+          -state: The State of the motor only possible values:"ON" to
+          run foward and "OF" to stop the motor
+       -version: 
+       0.1 
+       This function is to run the motor foward on the raspberry pi.
+       This function send a event to firebase to change the state of the motor 
+       and then in the Raspberry Pi the python script is set to listen to any change on the database 
+       to change the states of the Gpio Pins
+      */ 
+    func led(ChangedStateTo state: String){
+        /// Get a reference to the Firebase Database
         let ref = FIRDatabase.database().reference()
+        /// The object post to post in the database
         let post : [String: AnyObject] = ["state": state]
+        /// setValue Post of the child led in the Database
         ref.child("led").setValue(post)
         
         
